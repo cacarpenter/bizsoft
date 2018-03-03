f8888691ed2a2f3ccce56eaa56708cf89bd8572a
Craig Carpenter <cacarpe@gmail.com>
2018-03-02 18:46:55 -0700

    H2 stuff and change rest api

--- src/main/java/us/craigcarpenter/bizsoft/rest/BusinessRepository.java
+++ src/main/java/us/craigcarpenter/bizsoft/rest/BusinessRepository.java
@@ -2,13 +2,11 @@ package us.craigcarpenter.bizsoft.rest;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import us.craigcarpenter.bizsoft.model.Business;
 
-@RepositoryRestResource(collectionResourceRel = "business", path = "business")
 public interface BusinessRepository extends PagingAndSortingRepository<Business, Long> {
 
-        Business findByName(@Param("name") String name);
+	Business findByName(@Param("name") String name);
 
 }
--- src/main/java/us/craigcarpenter/bizsoft/rest/LocationRepository.java
+++ src/main/java/us/craigcarpenter/bizsoft/rest/LocationRepository.java
@@ -8,4 +8,6 @@ import us.craigcarpenter.bizsoft.model.Location;
 
 public interface LocationRepository extends PagingAndSortingRepository<Location, Long> {
 	List<Location> findByName(String name);
+
+	List<Location> findByBusiness(Long businessId);
 }
